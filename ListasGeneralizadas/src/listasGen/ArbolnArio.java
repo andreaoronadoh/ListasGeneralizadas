868c6545da0dcb332fab4fdd5fdca829a66dcf71
andreaoronadoh <72096677+andreaoronadoh@users.noreply.github.com>
2020-09-30 13:39:34 -0500

    Update ArbolnArio.java

--- ListasGeneralizadas/src/listasGen/ArbolnArio.java
+++ ListasGeneralizadas/src/listasGen/ArbolnArio.java
@@ -288,5 +288,72 @@ public class ArbolnArio {
         return raiz;
     }
 
+public void eliminar(Nodo cabeza, int dato, Nodo anterior_cabeza) {
 
+        Nodo Anterior, nodoactual;
+
+        nodoactual = cabeza;
+        Anterior = nodoactual;
+
+        boolean datoe = buscar(dato);
+
+        if (datoe == true) {
+
+            while (cabeza != null) {
+
+                /* llamado recursivo para acceder a la liga de la lista generalizada
+                Guardamos la cabeza de la lista generalizada para: si el dato a 
+                borrar es un nodo cabeza poder enlazarla sin perder los datos*/
+                if (cabeza.ligaLista != null) {
+
+                    eliminar(cabeza.ligaLista, dato, cabeza);
+                }
+
+                //Eliminamos si el dato es la raiz
+                if (dato == this.raiz.dato) {
+
+                    if (this.raiz.liga == null && this.raiz.ligaLista == null) {
+
+                        this.raiz = null;
+
+                    } else if (this.raiz.ligaLista != null) {
+
+                        this.raiz = this.raiz.ligaLista;
+
+                    } else {
+
+                        this.raiz = this.raiz.liga;
+                    }
+                } else if (nodoactual.dato == dato) {
+
+                    //eliminamos si el nodo es un nodo cabeza (lista de listas
+                    if (cabeza == Anterior) {
+
+                        anterior_cabeza.ligaLista = nodoactual.liga;
+                        nodoactual.liga = null;
+                        nodoactual = anterior_cabeza.ligaLista;
+                    } // eliminamos si es diferente a nulo y no es lista de listas
+                    else if (nodoactual.liga != null) {
+
+                        Anterior.liga = nodoactual.liga;
+                        nodoactual.liga = null;
+
+                        // eliminamos cuando el nodo esta apuntando a nulo
+                    } else if (nodoactual.liga == null) {
+
+                        Anterior.liga = null;
+
+                    }
+                }
+                //avanza en el ciclo y guarda el nodo anterior de la lista normal
+                cabeza = cabeza.liga;
+                Anterior = nodoactual;
+                nodoactual = cabeza;
+            }
+
+        } else {
+
+            JOptionPane.showMessageDialog(null, "Dato no encontrado");
+        }
+    } 
 }
